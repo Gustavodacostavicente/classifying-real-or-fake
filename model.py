adf891a5dcdc10ebb2772c15df3b628f9c4ab55a
Gustavodacostavicente <64452827+Gustavodacostavicente@users.noreply.github.com>
2024-09-07 20:01:10 -0300

    Update model.py

--- model.py
+++ model.py
@@ -20,14 +20,20 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
 }
 
 data_dir = 'dataset'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-                  for x in ['train', 'valid']}
+                  for x in ['train', 'valid', 'test']}
 dataloaders = {x: DataLoader(image_datasets[x], batch_size=32, shuffle=True, num_workers=4)
-               for x in ['train', 'valid']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+               for x in ['train', 'valid', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid', 'test']}
 class_names = image_datasets['train'].classes
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
